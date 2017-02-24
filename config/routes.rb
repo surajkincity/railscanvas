2200369c09f360e71f256b89989af8771ceb003a
Suraj Joshi <surajkincity@gmail.comit config --global user.email surajkincity@gmail.com>
2017-02-24 18:00:55 +0530

    Ce

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'home/index'
-  root 'welcome#index'
+  root 'home#index'
 
 
   # The priority is based upon order of creation: first created -> highest priority.
